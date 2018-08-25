431787d8302c5af937512d5f0a41ca0dc3badb72
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-08-26 02:24:28 +0530

    Camera following Ball

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	private Rigidbody rigidBody;
+	private AudioSource ballSound;
+	public Vector3 launchVelocity;
 	// Use this for initialization
 	void Start () {
-		
+		rigidBody = GetComponent<Rigidbody> ();
+		ballSound = GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+
+	public void Launch ()
+	{
+		rigidBody.velocity = launchVelocity;
+		ballSound.Play ();
 	}
 	
 	// Update is called once per frame
