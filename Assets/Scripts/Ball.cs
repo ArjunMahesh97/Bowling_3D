ae5d0b759628c2ca991df82e9bfc6dba2ca2802a
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-08-27 01:17:35 +0530

    Added panels

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+	private AudioSource ballSound;
+	public Vector3 launchVelocity;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		ballSound = GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+
+	public void Launch ()
+	{
+		rigidBody.velocity = launchVelocity;
+		ballSound.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
