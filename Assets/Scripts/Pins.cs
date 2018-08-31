78ef96297381f764d2fd4ca77b6c62ad040a2f76
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-09-01 01:47:25 +0530

    detecting angles

---
+++ Assets/Scripts/Pins.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pins : MonoBehaviour {
+	public float standingThreshold = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (name + IsStanding ());
+	}
+
+	public bool IsStanding(){
+		Vector3 rotationInEulur = transform.rotation.eulerAngles;
+
+		float tiltInX = Mathf.Abs (rotationInEulur.x);
+		float tiltInZ = Mathf.Abs (rotationInEulur.z);
+
+		if (tiltInX < standingThreshold && tiltInZ < standingThreshold)
+			return  true;
+		else
+			return	false;	
+	}
+}
