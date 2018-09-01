b342cf362c6984c9213c71540774a99b8c21ff63
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-09-01 16:54:22 +0530

    Counting standing pins

---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PinSetter : MonoBehaviour {
+
+	public Text standingDisplay;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		standingDisplay.text = CountStanding ().ToString ();
+		
+	}
+
+	int CountStanding(){
+		int standing = 0;
+		foreach (Pins pin in GameObject.FindObjectsOfType<Pins>()) {
+			if (pin.IsStanding ())
+				standing++;
+		} 
+		return standing;
+	}
+}
