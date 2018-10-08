709a760debf4a9a6ebc1680de339fba220cb4b37
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-10-08 20:22:29 +0530

    ScoreMaster Complete

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -22,8 +22,24 @@ public class ScoreMaster {
 		List<int> frames = new List<int> ();
 
 		for (int i = 1; i < rolls.Count; i += 2) {
+
+			if (frames.Count == 10) {
+				break;
+			}
+
 			if (rolls [1] + rolls [i - 1] < 10) {
 				frames.Add (rolls [i - 1] + rolls [i]);
+			}
+
+			if (rolls.Count - i <= 1) {
+				break;
+			}
+
+			if (rolls [i - 1] == 10) {
+				i--;
+				frames.Add (10 + rolls [i + 1] + rolls [i + 2]);
+			}else if (rolls [1] + rolls [i - 1] == 10) {
+				frames.Add (10 + rolls [i + 1]);
 			}
 		}
 
