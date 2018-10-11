ecae25ceb4c7b98184b07b1bb467b03e4425e78d
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-10-11 19:47:47 +0530

    New UI

--- Assets/Editor/ScoreMasterTest.cs
+++ Assets/Editor/ScoreMasterTest.cs
@@ -7,18 +7,18 @@ using System.Linq;
 [TestFixture]
 public class ScoreMasterTest {
 	
-	[Test]
-	public void T00PassingTest () {
-		Assert.AreEqual (1, 1);
-	}
-
-	[Test]
-	public void T01Bowl1 () {
-		int[] rolls = {1};
-		string rollsString = "1";
-		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList()));
-	}
-
+//	[Test]
+//	public void T00PassingTest () {
+//		Assert.AreEqual (1, 1);
+//	}
+//
+//	[Test]
+//	public void T01Bowl1 () {
+//		int[] rolls = {1};
+//		string rollsString = "1";
+//		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList()));
+//	}
+//
 //	[Test]
 //	public void T02BowlX () {
 //		int[] rolls = {10};
