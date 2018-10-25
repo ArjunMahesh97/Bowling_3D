a35cd43f0715096e96a8c791e0d2d0f65fc4f29c
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-10-25 21:31:06 +0530

    Completed ScoreDisplay

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -29,8 +29,24 @@ public class ScoreDisplay : MonoBehaviour {
 		}
 	}
 
-	static string FormatRolls(List<int> rolls){
+	public static string FormatRolls(List<int> rolls){
 		string output = "";
+
+		for (int i = 0; i < rolls.Count; i++) {
+			int box = output.Length + 1;
+
+			if (rolls [i] == 0) {
+				output += "-";
+			} else if (box % 2 == 0 && rolls [i - 1] + rolls [i] == 10) {
+				output += "/";
+			} else if (box >= 19 && rolls [i] == 10) {
+				output += "X";
+			} else if (rolls [i] == 10) {
+				output += "X ";
+			} else {
+				output += rolls [i].ToString ();
+			}
+		}
 
 		return output;
 	}
